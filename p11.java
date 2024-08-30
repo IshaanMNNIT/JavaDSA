78b025db1a8d2b8758235dacc3f567687388a6de
IshaanMNNIT <singhishaan2004@gmail.com>
2024-08-31 01:43:56 +0530

    Enhanced Switch

---
+++ p11.java
@@ -0,0 +1,23 @@
+import java.util.*;//Nested Switch Statements in JAVA
+class p11//Enhanced Switch alt+enter
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int empID = in.nextInt();
+        String department = in.next();
+
+        switch (empID) {
+            case 1 -> System.out.println("Kunal Kushwaha");
+            case 2 -> System.out.println("Rahul Rana");
+            case 3 -> {
+                System.out.println("Employee Number 3");
+                switch (department) {
+                    case "IT" -> System.out.println("IT Department");
+                    case "Management" -> System.out.println("Management Department");
+                    default -> System.out.println("No Department Available");
+                }
+            }
+            default -> System.out.println("Invalid Employee Number Added");
+        }
+    }
+}
