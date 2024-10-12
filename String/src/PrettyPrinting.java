c670e29e46ef0545fac1029316013b6e7fdde535
IshaanMNNIT <singhishaan2004@gmail.com>
2024-10-12 23:38:40 +0530

    String

---
+++ String/src/PrettyPrinting.java
@@ -0,0 +1,8 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        System.out.printf("Pi = %.3f",Math.PI);
+        System.out.println();
+        float s = 453.1879f;
+        System.out.printf("s = %.2f",s);
+    }
+}
