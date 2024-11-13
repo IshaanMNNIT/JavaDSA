05868375a286873e90ad752f4111b1da3aeeac83
IshaanMNNIT <singhishaan2004@gmail.com>
2024-11-13 17:35:50 +0530

    Quality Array

--- Arrays/src/QualityArray.java
+++ Arrays/src/QualityArray.java
@@ -7,7 +7,7 @@ public class QualityArray {
         Scanner in = new Scanner(System.in);int N = in.nextInt();
         int A[] = new int[N];
         ArrayList<Integer> list = new ArrayList<>(); // specify the type parameter (e.g., Integer)
-        System.out.println("Enter the Fuckin elements");
+        System.out.println("Enter the elements");
         for(int i = 0 ; i < N ; i++)
         {
             A[i] = in.nextInt();
@@ -35,20 +35,15 @@ public class QualityArray {
         }
         else
         {
-            int countodd=0;
+
             for(int i = 2 ; i < n ; i++)
             {
                 if(n%i==0 && i%2!=0)
                 {
-                    countodd++;
-                }
-
-                if(countodd >= 1)
                     return true;
-
+                }
             }
         }
-
         return false;
     }
 }
