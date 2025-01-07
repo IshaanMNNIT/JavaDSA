d5d5b2200fcfc0794aaf18a134f552aeb28a7322
IshaanMNNIT <singhishaan2004@gmail.com>
2025-01-07 15:52:23 +0530

    New Programs

--- Arrays/src/rotatingarray.java
+++ Arrays/src/rotatingarray.java
@@ -4,7 +4,23 @@ public class rotatingarray {
         Scanner in = new Scanner(System.in);
         int arr[] = new int[]{0,1,2,3,4,5,6,7,8};
         System.out.println("Enter the key index about which it needs to be rotated");
-        int key = in.nextInt();
-
+        int k = in.nextInt();
+        int l = arr.length;int temp;
+        if(k>l)
+        {
+            k = k % l;
+        }
+        for(int i=1;i<=k;i++)
+        {
+            for(int j=0;j<arr.length-1;j++)
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
+
+
 }
