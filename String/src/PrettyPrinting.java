c46bb8098c4a046185bdd45ca6f74f623eccc641
IshaanMNNIT <singhishaan2004@gmail.com>
2025-01-17 18:43:44 +0530

    Practicing Arrays

--- String/src/PrettyPrinting.java
+++
@@ -1,8 +0,0 @@
-public class PrettyPrinting {
-    public static void main(String[] args) {
-        System.out.printf("Pi = %.3f",Math.PI);
-        System.out.println();
-        float s = 453.1879f;
-        System.out.printf("s = %.2f",s);
-    }
-}
